e3aac05d4817e0873a1ecdb38ef029a32d4f66ab
LAPTOP-RQ4QRHU8\aidan <aidanclemente@gmail.com>
2018-01-13 11:20:08 -0500

    Interim Commit

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
+	$("#bodyContent").hide();
+
 	$("#submitButton").on("click", function() {
 		event.preventDefault();
+
+		$("#bodyContent").show();
 
 		var keyword = $("#searchInput").val().trim();
 		var ticketmasterKey = "RR18UORj62Wr84ro7p7UoiXyX9oRffwD";
@@ -15,47 +19,58 @@ $(document).ready(function () {
 		  dataType: "json",
 		  success: function(json) {
 		            $("#userData").empty();  
+		            console.log(json);
+		            // console.log(json._embedded.events);
 
-		            var events = json._embedded.events;
-		            console.log("events ", events);
 
-		            for (var i = 0; i < events.length; i++) {
-		             	
-						var eventName = events[i].name;
-						var eventTicketURL = events[i].url;
-						var eventVenue = events[i]._embedded.venues[0].name;
-						var venueURL = events[i]._embedded.venues[0].url;				
-						var eventCity = events[i]._embedded.venues[0].city.name;				
-						var eventState = events[i]._embedded.venues[0].state.stateCode;
-						var location = eventCity + ", " + eventState;
-						var eventDate = events[i].dates.start.localDate;
-						var formatDate = moment(eventDate).format("ddd L")
-						var eventTime = events[i].dates.start.localTime;
-						var convertedEventTime = moment(eventTime, "HH:mm A").format("h:mm A");
+		            if (json.page.totalElements == 0) {
 
-		//======================= Dynamic Elements ==========================
-			             var newRow = $("<tr>");
-			  			 var divEvent = $("<td><a class='link' href='" + eventTicketURL + "' target='_blank'>" +eventName + "</a></td>");
-			  			 var divLocation = $("<td><a class='link' href='" + venueURL + "' target='_blank'>" + eventVenue + "</a><br>" + location + "</td>");
-			  			 var divDate = $("<td>" + formatDate + "<br>" + convertedEventTime + "</td> ");
-			  			 var divTickets = $("<td>");
-			  			 var button = $("<a target='_blank'>");
+		            	console.log("FIRE!!!!!")
+		            	$("#ticketmasterDiv").html("We're Sorry, there are currently no " + keyword + " concerts. Please enjoy the videos and images!");
 
-			  			 button.text("Tickets");
-			  			 button.addClass("btn")
-			  			 button.attr("href", eventTicketURL);
-			  			 
-			  			 divTickets.append(button);
+		            } else if (json.page.totalElements > 0){
+		            	$("#ticketmasterDiv").show()
+			            var events = json._embedded.events;
+			            console.log("events ", events);
 
-			  			 newRow.addClass("searchReturn");
-			  			 divEvent.addClass("event col-sm-3");
-			  			 divLocation.addClass("event col-sm-3");
-			  			 divDate.addClass("event col-sm-3");
-			  			 divTickets.addClass("event col-sm-3");
+			            for (var i = 0; i < events.length; i++) {
+			             	
+							var eventName = events[i].name;
+							var eventTicketURL = events[i].url;
+							var eventVenue = events[i]._embedded.venues[0].name;
+							var venueURL = events[i]._embedded.venues[0].url;				
+							var eventCity = events[i]._embedded.venues[0].city.name;				
+							var eventState = events[i]._embedded.venues[0].state.stateCode;
+							var location = eventCity + ", " + eventState;
+							var eventDate = events[i].dates.start.localDate;
+							var formatDate = moment(eventDate).format("ddd L")
+							var eventTime = events[i].dates.start.localTime;
+							var convertedEventTime = moment(eventTime, "HH:mm A").format("h:mm A");
 
-			  			 $(newRow).append(divEvent, divLocation, divDate, divTickets);
-			  			 $("#userData").append(newRow);  
-					
+			//======================= Dynamic Elements ==========================
+				             var newRow = $("<tr>");
+				  			 var divEvent = $("<td><a class='link' href='" + eventTicketURL + "' target='_blank'>" +eventName + "</a></td>");
+				  			 var divLocation = $("<td><a class='link' href='" + venueURL + "' target='_blank'>" + eventVenue + "</a><br>" + location + "</td>");
+				  			 var divDate = $("<td>" + formatDate + "<br>" + convertedEventTime + "</td> ");
+				  			 var divTickets = $("<td>");
+				  			 var button = $("<a target='_blank'>");
+
+				  			 button.text("Tickets");
+				  			 button.addClass("btn")
+				  			 button.attr("href", eventTicketURL);
+				  			 
+				  			 divTickets.append(button);
+
+				  			 newRow.addClass("searchReturn");
+				  			 divEvent.addClass("event col-sm-3");
+				  			 divLocation.addClass("event col-sm-3");
+				  			 divDate.addClass("event col-sm-3");
+				  			 divTickets.addClass("event col-sm-3");
+
+				  			 $(newRow).append(divEvent, divLocation, divDate, divTickets);
+				  			 $("#userData").append(newRow);  
+						
+				  		}
 			  		}
 		    },
 		  	error: function(xhr, status, err) {
