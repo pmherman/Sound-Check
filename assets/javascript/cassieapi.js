0dbcb809c48b489b5faa510399e683c7a508b923
Cassie <cjrabe17@gmail.com>
2018-01-14 20:51:21 -0500

    Fixed API call

--- assets/javascript/cassieapi.js
+++ assets/javascript/cassieapi.js
@@ -1,32 +1,25 @@
-$(document).ready(function () {
-	$("#submitButton").on("click", function(event) {
-		event.preventDefault();
-		// $("h1").hide();
-		// $("form").prepend("<h2>SoundCheck</h2>");
-		// $(".jumbotron").addClass("no-padding");
-		// $(".lead").hide();
+$("#submitButton").on("click", function(event) {
+	event.preventDefault();
 
-		$(".carousel-inner").empty();
+	$(".carousel-inner").empty();
 
-		var input = $("#searchInput").val().trim();
-		console.log(input);
+	var input = $("#searchInput").val().trim();
+	console.log(input);
 
-		var apiURL = "https://api.500px.com/v1/photos/search?term=" + input + "&consumer_key=W7RBOrJkDlrlxglMjxP9hvbjth1uqeNrUX84jzBX&exclude_nude=1&only=concert&image_size=3";
+	var apiURL = "https://api.500px.com/v1/photos/search?term=" + input + "&consumer_key=W7RBOrJkDlrlxglMjxP9hvbjth1uqeNrUX84jzBX&exclude_nude=1&only=concert&image_size=3";
 
-		$.ajax({
-			url: apiURL,
-			method: "GET"
-		}).done(function(response) {
-			var results = response.photos;
-			console.log(response.photos);
+	$.ajax({
+		url: apiURL,
+		method: "GET"
+	}).done(function(response) {
+		var results = response.photos;
+		console.log(response.photos);
 
-			for (var i = 0; i < 10; i++) {
-				$(".carousel-inner").append("<div class='carousel-item polaroid'><img class='d-block img-responsive' src='" + results[i].image_url + "' alt= '" + results[i].name + "'</div><div class='carousel-caption d-block'><p>" + results[i].name + "</div></p>");
-				if (i == 0) {
-					$(".carousel-item").addClass("active");
-				}
+		for (var i = 0; i < 20; i++) {
+			$(".carousel-inner").append("<div class='carousel-item polaroid'><img class='d-block img-responsive' src='" + results[i].image_url + "' alt= '" + results[i].name + "'</div><div class='carousel-caption d-block'><p>" + results[i].name + "</div></p>");
+			if (i == 0) {
+				$(".carousel-item").addClass("active");
 			}
-		});
+		}
 	});
-
 });
