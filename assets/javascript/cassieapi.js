5405ddcfec70f83c7f59f624c23a573b4c5e5619
Cassie <cjrabe17@gmail.com>
2018-01-15 21:02:14 -0500

    Updated Jumbotron/navbar. Cleaned up cassieapi.js

--- assets/javascript/cassieapi.js
+++ assets/javascript/cassieapi.js
@@ -1,10 +1,15 @@
 $("#submitButton").on("click", function(event) {
 	event.preventDefault();
 
+	$("header").removeClass("full-screen");
+	$("header").addClass("navbar");
+	$(".lead").hide();
+	$("h1").removeClass("display-1 text-pop-up-top");
+	$("h1").addClass("display-4");
+
 	$(".carousel-inner").empty();
 
 	var input = $("#searchInput").val().trim();
-	console.log(input);
 
 	var apiURL = "https://api.500px.com/v1/photos/search?term=" + input + "&consumer_key=W7RBOrJkDlrlxglMjxP9hvbjth1uqeNrUX84jzBX&exclude_nude=1&only=concert&image_size=3";
 
@@ -13,7 +18,6 @@ $("#submitButton").on("click", function(event) {
 		method: "GET"
 	}).done(function(response) {
 		var results = response.photos;
-		console.log(response.photos);
 
 		for (var i = 0; i < 20; i++) {
 			$(".carousel-inner").append("<div class='carousel-item polaroid'><img class='d-block img-responsive' src='" + results[i].image_url + "' alt= '" + results[i].name + "'</div><div class='carousel-caption d-block'><p>" + results[i].name + "</div></p>");
